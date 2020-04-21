c437f78dc01799ed63e68bdd5f03093f4eee705c
William Morgan <37124296+ExtraLime@users.noreply.github.com>
2020-04-21 13:35:11 -0700

    Update server.js

--- timestampp/app/server.js
+++ timestampp/app/server.js
@@ -1,7 +1,3 @@
-// server.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
 
@@ -30,16 +26,25 @@ app.get("/api/timestamp/", function(req, res){
   })
 
 
-app.get("/api/timestamp/:date_string?", function (req, res){
-  const {date_string} = req.params;
-  var resp = {"error" : "Invalid Date" }
-  var date = new Date(date_string);  
-  if (date){
-    resp = {"unix": date.getTime(), "utc" : date.toUTCString()}
-  }
-  res.json(resp)
-})
+app.get("/api/timestamp/:date_string", (req, res) => {
+  let dateString = req.params.date_string;
 
+  //A 4 digit number is a valid ISO-8601 for the beginning of that year
+  //5 digits or more must be a unix time, until we reach a year 10,000 problem
+  if (/\d{5,}/.test(dateString)) {
+    var dateInt = parseInt(dateString);
+    //Date regards numbers as unix timestamps, strings are processed differently
+    res.json({ unix: dateString, utc: new Date(dateInt).toUTCString() });
+  }
+
+  let dateObject = new Date(dateString);
+
+  if (dateObject.toString() == "Invalid Date") {
+    res.json({error: "Invalid Date" });
+  } else {
+    res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
