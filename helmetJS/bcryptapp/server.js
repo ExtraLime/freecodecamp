96741d06e0c30aba6295a662cd890da4a736c502
ExtraLime <willdox7@live.com>
2020-04-21 18:42:09 -0700

    finished infosec

---
+++ helmetJS/bcryptapp/server.js
@@ -0,0 +1,30 @@
+'use strict';
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const app         = express();
+const bcrypt = require('bcrypt')
+
+fccTesting(app); //For FCC testing purposes
+
+const saltRounds = 12;
+const myPlaintextPassword = 'sUperpassw0rd!';
+const someOtherPlaintextPassword = 'pass123';
+
+
+
+
+//START_ASYNC -do not remove notes, place code between correct pair of notes.
+
+//END_ASYNC
+
+//START_SYNC
+let hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+let result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
+
+//END_SYNC
+
+
+app.listen(process.env.PORT || 3000, () => {});
